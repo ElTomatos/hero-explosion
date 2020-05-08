b00568c1c0bdaffc62481e0e3fe19a86696683b4
Sergey Petrov <eltmt@yandex.ru>
2020-05-08 21:47:24 +0300

    init and build config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/javascript/app.ts',
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
+      }
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    }),
+  ],
+
+  devtool: 'cheap-eval-source-map',
+};
